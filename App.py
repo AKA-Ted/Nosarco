0bf1531d6621e3f9197771b9ac1a640bfccb3ec0
Sandy Cespedes <cespedesmonserrat4@gmail.com>
2022-04-30 03:09:37 -0500

    Cambios en implementacion de librería para mysql

--- App.py
+++ App.py
@@ -1,10 +1,9 @@
+from typing import final
 from flask import Flask, render_template, request, redirect, url_for, flash, session
-from flask_mysqldb import MySQL
-from dotenv import load_dotenv
+from flaskext.mysql import MySQL
+import pymysql
 import os
-import json
 
-load_dotenv()
 app = Flask(__name__)
 
 app.secret_key = os.getenv("SECRET_KEY")
@@ -12,13 +11,13 @@ app.config.from_object(__name__)
 
 app.secret_key = "flash message"
 
-#CONFIGURACIONES DE MYSQL
-app.config['MYSQL_HOST'] = 'bv2rebwf6zzsv341.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
-app.config['MYSQL_USER'] = 'rzh1cxyhp1eiwu5g'
-app.config['MYSQL_PASSWORD'] = 'dlz55fjlmcun57qn'
-app.config['MYSQL_DB'] = 'raia64mmpqm1hqos'
-  
-mysql = MySQL(app)
+#MySQL
+con = pymysql.connect(host='bv2rebwf6zzsv341.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user='rzh1cxyhp1eiwu5g',
+        password='dlz55fjlmcun57qn',
+        db='raia64mmpqm1hqos')
+mysql = MySQL()
+mysql.init_app(app)
 
 #ENRUTAMIENTO
 @app.route('/', methods = ['POST', 'GET'])
@@ -34,23 +33,22 @@ def Login():
             num_empleado = request.form['num_empleado']
             password = request.form['password']
 
-            cur = mysql.connection.cursor()
-            cur.execute(F"CALL sp_login(\"{num_empleado}\",\"{password}\")")
-            data = cur.fetchall()
-            cur.close()
-            mysql.connection.commit()
+            with con.cursor() as cursor:
+                cursor.execute(F"CALL sp_login(\"{num_empleado}\",\"{password}\")") 
+                data = cursor.fetchall()
+                con.commit()
+                con.close()
 
-            if data[0][0] != 'NO INICIAR' :
-                session['num_empleado'] = num_empleado
-                session['id_usuario'] = data[0][0]
-                session['is_admin'] = True if (data[0][1] == 0) else False
-                return redirect(url_for('Index'))
-            else:
-                flash("Datos incorrectos")
-                return redirect(url_for('Login'))
+                if data[0][0] != 'NO INICIAR' :
+                    session['num_empleado'] = data[0][0]
+                    session['is_admin'] = True if (data[0][1] == 0) else False
+                    return redirect(url_for('Index'))
+                else:
+                    flash("Datos incorrectos")
+                    return redirect(url_for('Login'))
         except:
-             flash("Error")
-             return redirect(url_for('Login')) 
+            flash('Error')
+            return redirect(url_for('Login')) 
 
 
 @app.route('/logout', methods = ['GET'])
@@ -59,15 +57,11 @@ def Logout():
         session.pop('num_empleado', None)
         session.pop('id_usuario', None)
         return redirect(url_for('Login'))
-      
-
 
 @app.route('/index', methods = ['POST', 'GET'])
 def Index():
     if 'num_empleado' not in session:
         return redirect(url_for('Login'))
-
-    id_user = session['id_usuario']
 
     return render_template('index.html', user = session)
 
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # FarmaciaNosarco
-Farmacia Nosarco
+Farmacia Nosarc
+
+Para poder ejecutar el código deberás escribir primero en la consola
+
+```
+pip install flask
+pip install PyMySQL
+```
